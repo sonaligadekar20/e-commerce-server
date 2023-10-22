540f15548790f1a532eddd72b3c82ee9770433f9
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-10-22 14:01:02 +0530

    added put & patch end points

--- index.js
+++ index.js
@@ -72,7 +72,60 @@ app.delete('/product/:_id', async (req, res) => {
     })
 });
 
+app.put('/product/:_id', async (req, res) => {
+    const { _id } = req.params;
+    const { name, description, price, productImage, brand } = req.body;
+    await product.updateOne(
+        { _id : _id },
+        { $set: {
+                name: name,
+                description: description,
+                price: price,
+                productImage: productImage,
+                brand: brand,
+            }
+        }
+    );
 
+    const updatedproduct = await product.findOne({ _id: _id });
+    res.json({
+        success: true,
+        data: updatedproduct,
+        message: `Successfully updated new product`,
+    })
+});
+
+app.patch('/product/:_id', async(req, res)=>{
+    const {_id} = req.params;
+    const { name, description, price, productImage, brand } = req.body;
+
+    const Product = await product.findById(_id);
+
+    if(name){
+        Product.name = name;    
+    }
+    if(description){
+        Product.description = description;  
+    }
+    if(price){
+        Product.price = price;  
+    }
+    if(brand){
+        Product.brand = brand;  
+    }
+    if(productImage){
+        Product.productImage =productImage;  
+    }
+
+    const updatedproduct =await Product.save();
+
+    res.json({
+        success:true,
+        data:updatedproduct,
+        message:`Successfully updated`,
+    })
+    
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
